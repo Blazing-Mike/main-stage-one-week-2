1ca3aa83abebd934e7dcb86f5ce378d2d7f53cba
Blazing-mike <adebambomichael5@gmail.com>
2024-04-30 00:51:15 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const API_URL =
+  "https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "169fe60703mshe88b7af446e7bd9p130d2cjsnabbed6375b91",
+    "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+  },
+};
+const propertyListings = document.getElementById("propertiesList");
+
+fetch(API_URL, options)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    displayProperties(data?.hits);
+  })
+  .catch((error) => {
+    console.error("FETCH ERROR:", error);
+    displayError("NETWORK RESPONSE ERROR");
+  });
+
+function displayProperties(properties) {
+  properties.forEach((property) => {
+    const propertyElement = document.createElement("div");
+    propertyElement.className = "property";
+
+    const location = document.createElement("p");
+    location.textContent =
+      property.location[0].name + ", " + property.location[1].name;
+    location.className = "location";
+
+    const title = document.createElement("h2");
+    let formattedTitle = property.title.split('||')[0].trim();
+    title.textContent = formattedTitle;
+    title.className = "title";
+
+    const price = document.createElement("p");
+    price.textContent = "$" + property.price;
+    price.className = "price";
+
+    const propertyImage = document.createElement("img");
+    propertyImage.src = property.coverPhoto.url;
+    propertyImage.className = "property-image";
+
+    propertyElement.appendChild(propertyImage);
+    propertyElement.appendChild(title);
+    propertyElement.appendChild(price);
+    propertyElement.appendChild(location);
+    propertyListings.appendChild(propertyElement);
+  });
+}
+
+function displayError(message) {
+  const errorElement = document.createElement("div");
+  errorElement.className = "error";
+  errorElement.textContent = message;
+  propertyListings.appendChild(errorElement);
+}
